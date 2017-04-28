fd1ab4c3e3260a67f3da76843b20ce9d506793e8
Ryan-Larkin <ryan.r.larkin@gmail.com>
2017-04-28 12:59:45 -0400

    Update moderator.sql

--- database/moderator.sql
+++ database/moderator.sql
@@ -2,9 +2,3 @@ ALTER TABLE subreddits
 FOREIGN KEY (moderatorId) REFERENCES users (id) ON DELETE SET NULL;
 
 ALTER TABLE subreddits ADD CONSTRAINT fk_moderator_id FOREIGN KEY (moderatorId) REFERENCES users(id);
-
-
-
-moderatorId guillaume = 1390
-
-UPDATE subreddits SET moderatorId = 1390
